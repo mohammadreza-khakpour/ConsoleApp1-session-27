6fa83c903547096a218b51b4968922fb71a7d328
f14-tomcat <rezakhakpoorboraz@gmail.com>
2020-04-23 00:54:55 -0700

    final commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,17 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             var engine = Python.CreateEngine();
-            var source = engine.CreateScriptSourceFromFile(@"C: \Users\ASP_Reza\source\repos\ConsoleApp1\PythonApplication1 - 27\PythonApplication1_27.py");
+            var source = engine.CreateScriptSourceFromFile(@"C:\Users\ASP_Reza\source\repos\ConsoleApp1\PythonApplication1-27\PythonApplication1_27.py");
+            var scope = engine.CreateScope();
+            source.Execute(scope);
+            // loads the class definition to the memory:
+            dynamic class1 = scope.GetVariable("MohammadpourPython");
+            // making an object of my class:
+            dynamic obj = engine.Operations.CreateInstance(class1);
+
+            Console.WriteLine(obj.Sum(10,20));
+            Console.WriteLine(obj.Mult(10,20));
+
         }
     }
 }
